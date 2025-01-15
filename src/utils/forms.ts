85ddeda775446101f31e5775cdd3c57d7f17c3bc
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-01-15 11:46:27 -0600

    Se agrega dependencia lectura de markdown

---
+++ src/utils/forms.ts
@@ -0,0 +1,48 @@
+// Styles ---------------------------------------------------------------------------------
+export const inputStyles : string = "w-full field-input border-2 border-secondary rounded-xl p-3 text-secondary h-12"
+export const labelStyles : string = "ml-1"
+export const errorStyles : string = "text-red-500 ml-2 text-xs"
+
+//  Form ---------------------------------------------------------------------------------
+export type FormKeys = 'full_name' | 'phone' | 'date_answer';
+export type FormInitialState = Record<FormKeys, string>;
+
+export const formInitialState : FormInitialState = {
+    full_name: '',
+    phone: '', 
+    date_answer: ''
+}
+
+export const sendInitialState = {
+    loading:false,
+    status:-1
+}
+
+// Regex ---------------------------------------------------------------------------------
+type RegexOptions = 'optional_full_name' | 'required_phone' | 'optional_date_answer' | 'required_date_answer' | 'privacy';
+type RegexList = Record<RegexOptions, RegExp>;
+
+export const regex : RegexList = {
+    optional_full_name: new RegExp(/^$|^[A-Za-zÁÉÍÓÚáéíóúñÑ]{2,}(?:\s[A-Za-zÁÉÍÓÚáéíóúñÑ'`]{2,})*$/),
+    required_phone : new RegExp(/^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{3,4}[-.\s]?\d{4,6}$/),
+    optional_date_answer : new RegExp(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$|^$/),
+    required_date_answer : new RegExp(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/),
+    privacy: new RegExp(/^true$/), 
+}
+
+export const styles = {
+
+}
+
+// helpers ---------------------------------------------------------------------------------
+
+export const removeEmptyKeys = (obj) => {
+    for (const key in obj) {
+      if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
+        delete obj[key];
+      }
+    }
+    return obj;
+};
+
+export const whatsAppUrl = 'https://wa.me/522221275134?text=Hola%20Estaba%20en%20tu%20sitio%20y%20quiero%20información%20sobre%20el%20infblable.'
