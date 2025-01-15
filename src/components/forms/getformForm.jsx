e446c614773f869df6e86a05384d1123ca6bade3
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-01-15 14:39:20 -0600

    Cambio boton enviar

--- src/components/forms/getformForm.jsx
+++ src/components/forms/getformForm.jsx
@@ -17,12 +17,12 @@ const formErrorsInitialState = {
         status:true,
         regex: regex.optional_full_name,
         message:'El nombre solo puedo contener letras y espacios'
-    }, 
+    },
     phone: {
         status:false,
         regex: regex.required_phone,
         message:'El whastapp debe ser un numero valido'
-    }, 
+    },
     date_answer: {
         status:true,
         regex: regex.required_date_answer,
@@ -42,7 +42,7 @@ const formErrorsInitialState = {
 const Stack = ({children, value, error}) => (
     <div className="flex flex-col gap-1">
         {children}
-        <div className="h-4 -mt-1">            
+        <div className="h-4 -mt-1">
             {   value && !error?.status && <span className={errorStyles}>{error?.message}</span> }
         </div>
     </div>
@@ -66,8 +66,8 @@ const SnackBar = ({text, open, handleClose, status = 'info'}) => {
     },[open])
 
     return (
-        <div className={`fixed -top-10 z-50 flex items-center justify-center 
-            w-full max-w-[400px] gap-3 rounded-lg p-2 shadow-xl 
+        <div className={`fixed -top-10 z-50 flex items-center justify-center
+            w-full max-w-[400px] gap-3 rounded-lg p-2 shadow-xl
             ${statusStates[status]}
             ${open ? 'translate-y-20 ' : '-translate-y-full'} transition-all duration-300 ease-in-out`
         }>
@@ -81,7 +81,7 @@ const SnackBar = ({text, open, handleClose, status = 'info'}) => {
 
 // -----------------------------------------------------------------------------
 const ContactForm = () => {
-    
+
     const [form, setForm] = useState(formInitialState);
     const [formErrors, setFormErrors] = useState(formErrorsInitialState);
     const [send, setSend] = useState(sendInitialState);
@@ -90,16 +90,16 @@ const ContactForm = () => {
     const [open, setOpen] = useState(false);
     const handleClose = () => setOpen(false);
     const handleOpen = () => setOpen(true);
-    
+
     useEffect(()=>{
         const newErrors = {...formErrors}
-        Object.keys(form).forEach(key => {        
+        Object.keys(form).forEach(key => {
             const isValid = formErrors[key]?.regex.test(form[key])
             newErrors[key].status = isValid
         })
         setFormErrors(newErrors)
     },[form])
-    
+
     const handleBlur =  e => {
         e.preventDefault();
         const {target : {name, value}} = e
@@ -107,7 +107,7 @@ const ContactForm = () => {
         newForm[name] = value.trim()
         setForm(newForm)
     }
-    
+
     const setMessage = (text, status = 'info') => {
         setMsg({text,status})
         setOpen(true)
@@ -146,36 +146,36 @@ const ContactForm = () => {
 
 return (
     <section className="flex flex-col items-center justify-center" >
-        
-        <SnackBar 
+
+        <SnackBar
             text={msg.text}
             status={msg.status}
-            open={open} 
-            handleClose={handleClose} 
+            open={open}
+            handleClose={handleClose}
             handleOpen={handleOpen}
         />
-        
+
         <h3 className="text-2xl max-w-[500px] font-bold my-10 text-center px-5">Déjanos la información de tu evento y te contactaremos</h3>
-        <div className="flex flex-col items-center justify-center py-10 px-4 sm:px-6 lg:px-8 w-[min(90vw,500px)] md:w-[min(90vw,1000px)] 
+        <div className="flex flex-col items-center justify-center py-10 px-4 sm:px-6 lg:px-8 w-[min(90vw,500px)] md:w-[min(90vw,1000px)]
         border-2 border-secondary rounded-xl mb-10 shadow-md text-secondary dark:text-default">
-        
+
             <form id="contactForm" className="w-full">
-                
+
                 <fieldset className="flex flex-col gap-3" aria-labelledby='general-data'>
 
                     <legend id='general-data' className='text-xl mb-5 font-bold'>
                         Información de contacto
-                        <span className="block text-xs text-default font-normal"> (*) Obligatorios</span>    
+                        <span className="block text-xs text-default font-normal"> (*) Obligatorios</span>
                     </legend>
-                    
+
                     <Stack error={formErrors.full_name} value={form.full_name}>
                         <label className={labelStyles} htmlFor="full_name" id='full-name-label' > Nombre</label>
-                        <input 
+                        <input
                             type="text"
                             value={form.full_name}
                             onChange={(e) => setForm({...form, full_name: e.target.value})}
-                            name="full_name" 
-                            aria-labelledby="full-name-label" 
+                            name="full_name"
+                            aria-labelledby="full-name-label"
                             placeholder="Escribe tu nombre como Juan o María"
                             className={`${inputStyles} ${formErrors.full_name?.status && form.full_name ? 'border-green-600' : form.full_name && 'border-red-500'}`}
                             onBlur={e => handleBlur(e)}
@@ -185,8 +185,8 @@ return (
 
                     <Stack error={formErrors.phone} value={form.phone}>
                         <label className={labelStyles} id='phone-label' htmlFor="phone">Whatsapp * </label>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             name="phone"
                             value={form.phone}
                             onChange={(e) => setForm({...form, phone: e.target.value})}
@@ -199,12 +199,12 @@ return (
 
                     <Stack error={formErrors.date_answer} value={form.date_answer}>
                         <label className={labelStyles} id='date-label' htmlFor="date_answer"> Fecha de interes * </label>
-                        <input 
-                            type="date" 
+                        <input
+                            type="date"
                             value={form.date_answer}
                             onChange={(e) => setForm({...form, date_answer: e.target.value})}
                             aria-labelledby="date-label"
-                            name="date_answer" 
+                            name="date_answer"
                             placeholder="Escribe la fecha de tu evento"
                             className={`${inputStyles} ${formErrors.date_answer?.status && form.date_answer ? 'border-green-600' : form.date_answer && 'border-red-500'}`}
                         />
@@ -225,12 +225,13 @@ return (
                 </Stack>
 
 
-                <button type="submit" onClick={handleSend} className="mt-5 btn-secondary w-[100%] focus:outline-none focus:ring-0">
+                <button type="submit" onClick={handleSend} className="flex items-center justify-center mt-5 btn-secondary w-full focus:outline-none focus:ring-0 text-2xl">
                     {
-                        send.loading 
-                        ? <Icon icon="tabler:loader-3" width="24" height="24" className='animate-spin p-3' />
-                        : <Icon icon="tabler:send" width="24" height="24" />}
-                    Enviar
+                        send.loading
+                        ? <Icon icon="tabler:loader-2" width="24" height="24" className='animate-spin' />
+                            : 'Enviar'
+                    }
+
                 </button>
             </form>
         </div>
