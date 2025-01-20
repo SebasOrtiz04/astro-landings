0147a95ea072dde2c4f0fe0990ffceb307281f71
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-01-20 09:51:59 -0600

    Se hace limpieza de index

---
+++ src/data/pasos-contratar.ts
@@ -0,0 +1,30 @@
+export interface PasosContratar {
+  title: string;
+  description?: string;
+  icon: string;
+}
+
+export const pasosContratar: Array<PasosContratar> = [
+  {
+    title: 'Paso 1: <span class="font-medium">Contáctanos</span>',
+    description:
+      "Mandanos mensaje por whatsapp o dejanos tu contacto y fecha de interes y nos pondremos en contacto contigo.",
+    icon: 'tabler:brand-whatsapp',
+  },
+  {
+    title: 'Paso 2: <span class="font-medium">Verifica disponibilidad</span>',
+    description:
+      "Confirma la disponibilidad para el día de tu fiesta.",
+    icon: 'tabler:calendar-check',
+  },
+  {
+    title: 'Paso 3: <span class="font-medium">Aparta la fecha</span>',
+    description:
+      'Tenemos pago con efectivo, y transferencia. Elije la que mejor te acomode',
+    icon: 'tabler:paywall',
+  },
+  {
+    title: 'Todo listo para tu fiesta!',
+    icon: 'tabler:check',
+  },
+];
