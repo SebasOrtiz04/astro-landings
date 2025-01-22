1b0e15a4682345a3d05d3f4beb61d8d97546339a
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-01-22 10:54:34 -0600

    SE agrega condicion de mostrar flechas

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import { useState, useCallback } from "react";
 
 const ArrowStyle = "bg-primary text-white rounded-full w-5 h-5 p-2 md:w-10 md:h-10 md:p-3 shadow-lg hover:bg-primary-dark";
 
-const Carousel = ({ slides }) => {
+const Carousel = ({ slides, showArrows }) => {
   const [currentPhoto, setCurrentPhoto] = useState(0);
 
   const previousPhoto = useCallback(() => {
@@ -32,14 +32,17 @@ const Carousel = ({ slides }) => {
             />
           ))}
         </div>
-        <div className="absolute top-0 h-full w-full justify-between items-center flex px-2">
-          <button onClick={previousPhoto} aria-label="Previous Slide">
-            <Icon icon="tabler:chevron-left" className={ArrowStyle} width="24" height="24" />
-          </button>
-          <button onClick={nextPhoto} aria-label="Next Slide">
-            <Icon icon="tabler:chevron-right" className={ArrowStyle} width="24" height="24" />
-          </button>
-        </div>
+        {
+          showArrows &&
+          <div className="absolute top-0 h-full w-full justify-between items-center flex px-2">
+            <button onClick={previousPhoto} aria-label="Previous Slide">
+              <Icon icon="tabler:chevron-left" className={ArrowStyle} width="24" height="24" />
+            </button>
+            <button onClick={nextPhoto} aria-label="Next Slide">
+              <Icon icon="tabler:chevron-right" className={ArrowStyle} width="24" height="24" />
+            </button>
+          </div>
+        }
         <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
           {slides.map((s, i) => {
             return (
