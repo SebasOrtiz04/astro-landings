a9f6f0912be1fa74a74d56312a123c6210a85213
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-01-22 10:18:08 -0600

    Se hace carousel

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,63 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { useState, useCallback } from "react";
+
+const ArrowStyle = "bg-primary text-white rounded-full w-5 h-5 p-2 md:w-10 md:h-10 md:p-3 shadow-lg hover:bg-primary-dark";
+
+const Carousel = ({ slides }) => {
+  const [currentPhoto, setCurrentPhoto] = useState(0);
+
+  const previousPhoto = useCallback(() => {
+    setCurrentPhoto(currentPhoto === 0 ? slides.length - 1 : currentPhoto - 1);
+  }, [currentPhoto, slides.length]);
+
+  const nextPhoto = useCallback(() => {
+    setCurrentPhoto(currentPhoto === slides.length - 1 ? 0 : currentPhoto + 1);
+  }, [currentPhoto, slides.length]);
+
+  return (
+    <div className="w-[60%] m-auto">
+      <div className="overflow-hidden relative">
+        <div
+          className="flex transition ease-out duration-300"
+          style={{
+            transform: `translateX(${-currentPhoto * 100}%)`,
+          }}
+        >
+          {slides.map((slide, key) => (
+            <img
+              src={slide.src}
+              alt={slide.alt || `Slide ${key + 1}`}
+              key={key}
+              className="w-full object-cover"
+            />
+          ))}
+        </div>
+        <div className="absolute top-0 h-full w-full justify-between items-center flex px-2">
+          <button onClick={previousPhoto} aria-label="Previous Slide">
+            <Icon icon="tabler:chevron-left" className={ArrowStyle} width="24" height="24" />
+          </button>
+          <button onClick={nextPhoto} aria-label="Next Slide">
+            <Icon icon="tabler:chevron-right" className={ArrowStyle} width="24" height="24" />
+          </button>
+        </div>
+        <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+          {slides.map((s, i) => {
+            return (
+              <div
+                onClick={() => {
+                  setCurrentPhoto(i);
+                }}
+                key={"circle" + i}
+                className={`rounded-full w-5 h-5 cursor-pointer  ${
+                  i == currentPhoto ? "bg-white" : "bg-gray-500"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/data/carousel-photos.ts
@@ -0,0 +1,16 @@
+export interface CarouselPhoto {
+  src: string;
+  alt?: string;
+}
+
+export const carouselPhotos: Array<CarouselPhoto> = [
+    {
+      src: 'https://images.unsplash.com/photo-1597423498219-04418210827d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1597423498219-04418210827d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1597423498219-04418210827d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80',
+    },
+];
