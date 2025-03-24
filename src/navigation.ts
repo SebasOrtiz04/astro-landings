9afa7b3d6dff9a8f9aa8a684fac84cec5a7db6df
Juan Sebastian Ortiz Castro <93163636+SebasOrtiz04@users.noreply.github.com>
2025-03-23 22:50:10 -0600

    maquetando landing

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,38 +1,24 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-  // links: [
-  //   {text: 'Inicio', href: '/',},
-  //   {text: 'Nosotros', href: '/#',},
-  //   {text: 'Servicios', href: '/#',},
-  //   {text: 'Contácto', href: '/#',},
-  // ],
+  links: [
+    {text: 'Inicio', href: '/',},
+    {text: 'Nosotros', href: '/#',},
+    {text: 'Servicios', href: '/#',},
+    {text: 'Contácto', href: '/#',},
+  ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
-  links: [
-    // {
-    //   title: 'Company',
-    //   links: [
-    //     { text: 'About', href: '#' },
-    //     { text: 'Blog', href: '#' },
-    //     { text: 'Careers', href: '#' },
-    //     { text: 'Press', href: '#' },
-    //     { text: 'Inclusion', href: '#' },
-    //     { text: 'Social Impact', href: '#' },
-    //     { text: 'Shop', href: '#' },
-    //   ],
-    // },
-  ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    // { text: 'Terms', href: getPermalink('/terms') },
+    // { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
